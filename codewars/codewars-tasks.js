de3eee038fa6a83c3d8075091c1a2a43bc25e525
RomanMishchykha <91611433+RomanMishchykha@users.noreply.github.com>
2023-08-18 13:36:30 +0300

    Update: codewars-tasks.js add result to Make negative

--- codewars/codewars-tasks.js
+++ codewars/codewars-tasks.js
@@ -20,3 +20,11 @@ function lovefunc(flower1, flower2){
   }
   else return false
 }
+
+//https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+function makeNegative(num) {
+  if (num > 0) {
+    return  -num 
+  }
+  else return num
+}
