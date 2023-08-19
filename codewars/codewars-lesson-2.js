f45e49c710b3ac264d9d1147a1bc4f0f3a173853
MishaPolishchuk <polishchuk.misha.348@gmail.com>
2023-08-19 19:30:15 +0300

    Add lesson-2 task-2 solution

--- codewars/codewars-lesson-2.js
+++ codewars/codewars-lesson-2.js
@@ -4,3 +4,18 @@
 function stringToStringArray(string) {
     return string.split(/[ !@#$%^&*()_+=:"?><]/);
 }
+
+// DNA to RNA converter
+https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+
+function dnaToRnaConverter(dna){
+    if(dna.length === 0 || dna.indexOf("T") === -1)
+        return dna;
+
+    let dnaAsArray = dna.split("");
+    for(let i = 0; i<dnaAsArray.length; i++){
+        if(dnaAsArray[i] === "T")
+            dnaAsArray[i] = "U";
+    }
+    return dnaAsArray.join("");
+}
