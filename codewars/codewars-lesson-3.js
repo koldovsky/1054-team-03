2a67b2b985707abe1f3f67f316ff48ed9a97ad6f
MishaPolishchuk <polishchuk.misha.348@gmail.com>
2023-08-23 17:24:54 +0300

    Add lesson-3 task-5 solution

--- codewars/codewars-lesson-3.js
+++ codewars/codewars-lesson-3.js
@@ -37,3 +37,60 @@ function giveMeFive(obj) {
     }
     return resultArray;
 }
+
+// Inheritance
+// https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript
+
+class Animal {
+    constructor(name, age, legs, species, status) {
+        this.name = name;
+        this.age = age;
+        this.legs = legs;
+        this.species = species;
+        this.status = status;
+    }
+    introduce() {
+        return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+    }
+}
+
+class Shark extends Animal {
+    constructor(name, age, status) {
+        super(name, age, status);
+        this.legs = 0;
+        this.species = "shark";
+        this.status = status;
+    }
+}
+
+class Cat extends Animal {
+
+    constructor(name, age, status) {
+        super(name, age, status);
+        this.legs = 4;
+        this.species = "cat";
+        this.status = status;
+    }
+
+    introduce() {
+        return `Hello, my name is ${this.name} and I am ${this.age} years old.  Meow meow!`;
+    }
+}
+
+class Dog extends Animal {
+    constructor(name, age, status, master) {
+        super(name, age, status);
+        this.master = master;
+        this.legs = 4;
+        this.species = "dog";
+        this.status = status;
+    }
+
+    greetMaster() {
+        return `Hello ${this.master}`;
+    }
+
+    introduce() {
+        return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+    }
+}
