4baa9762524e351d05a39e68ea0cd618c6988a74
MishaPolishchuk <polishchuk.misha.348@gmail.com>
2023-09-05 22:48:54 +0300

    Add subscription aprove responce modal

---
+++ js/modals/news-subscription-aprove-responce-modal.js
@@ -0,0 +1,5 @@
+export const okBtn = document.querySelector(".ok-btn");
+// Get the modal
+export let modal = document.getElementById("myModal");
+// Get the <span> element that closes the modal
+export let span = document.getElementsByClassName("close")[0];
--- js/news-subscription.js
+++ js/news-subscription.js
@@ -1,8 +1,26 @@
+import * as subscriptionAproveResponceModalComponents from "./modals/news-subscription-aprove-responce-modal.js";
+
 const email = document.getElementById("email");
 const form = document.getElementById("form");
 const errorMsg = document.getElementById("error-msg-id");
 const submitBtn = document.querySelector(".subscription-card__submit-btn");
 const submitBtnText = document.querySelector(".submit-btn-text");
+
+// When the user clicks cross of the modal, close it
+subscriptionAproveResponceModalComponents.span.onclick = function () {
+    subscriptionAproveResponceModalComponents.modal.style.display = "none";
+}
+// When the user clicks OK of the modal, close it
+subscriptionAproveResponceModalComponents.okBtn.onclick = function () {
+    subscriptionAproveResponceModalComponents.modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == subscriptionAproveResponceModalComponents.modal) {
+        subscriptionAproveResponceModalComponents.modal.style.display = "none";
+    }
+}
 
 const isValidEmail = email => {
     const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -10,20 +28,29 @@ const isValidEmail = email => {
 }
 
 form.addEventListener("submit", (e) => {
-    if(email.value === "" || email.value == null){
-        e.preventDefault();       
-        errorMsg.innerText = "Email is required for subscription";       
+    if (email.value === "" || email.value == null) {
+        e.preventDefault();
+        errorMsg.innerText = "Email is required for subscription";
     }
 
-    else if(email.value.length > 0){      
-        if(!isValidEmail(email.value)){           
+    else if (email.value.length > 0) {
+        if (!isValidEmail(email.value)) {
             errorMsg.innerText = "Provide valid email address";
             e.preventDefault();
-            return;        
+            return;
         }
         e.preventDefault();
 
+        console.log('was here');
+        errorMsg.innerText = "";
         submitBtnText.innerText = "";
         submitBtn.classList.add("btn-loading");
+
+        setTimeout(() => {
+            subscriptionAproveResponceModalComponents.modal.style.display = "block";
+            submitBtn.classList.remove("btn-loading");
+            submitBtnText.innerText = "Submit";
+            email.value = "";
+        }, 2500);
     }
-})
+});
